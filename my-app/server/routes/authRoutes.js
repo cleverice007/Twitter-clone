b9d33a92f902205fe76674af3faeab92d0a6acde
cleverice007 <cleverice90128@gmail.com>
2023-08-05 17:36:06 +0800

    auth route and bcrypt

--- my-app/server/routes/authRoutes.js
+++ my-app/server/routes/authRoutes.js
@@ -3,9 +3,17 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const authControllers = require('../controllers/auth');
+const passport = require('passport');
 
 // 註冊
 router.post('/register', authControllers.register);
+
+//登入
+router.post(passport.authenticate('local', { failureRedirect: '/login' }), authControllers.login)
+
+//登出
+router.get('/logout',authControllers.logout)
+
 
 
 module.exports = router;
