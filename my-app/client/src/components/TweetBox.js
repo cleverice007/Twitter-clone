2cc186badc2e177260ba3e76d8ffd27629114b56
cleverice007 <cleverice90128@gmail.com>
2023-08-09 12:46:56 +0800

    將 tweetbox 內容傳去後端（create tweet)

--- my-app/client/src/components/TweetBox.js
+++ my-app/client/src/components/TweetBox.js
@@ -4,9 +4,26 @@ import styles from '../css/TweetBox.module.css';
 const TweetBox = ({ onPost }) => {
   const [tweetInput, setTweetInput] = useState("");
 
-  const handlePost = () => {
-    onPost(tweetInput);
-    setTweetInput("");
+  const handlePost = async () => {
+    try {
+      const response = await fetch('http://localhost:4000/createTweet', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ content: tweetInput })
+      });
+      
+      if (!response.ok) {
+        throw new Error('Failed to post tweet');
+      }
+
+      const data = await response.json();
+      onPost(data);  // Inform the parent about the new tweet
+      setTweetInput("");
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
   }
 
   return (
@@ -23,3 +40,4 @@ const TweetBox = ({ onPost }) => {
 }
 
 export default TweetBox;
+
