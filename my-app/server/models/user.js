aafbd8fbc4e7361edf136ae79b7a0aedc4f35aac
cleverice007 <cleverice90128@gmail.com>
2023-08-24 11:31:43 +0800

    新增 頭貼、自介欄位 到user schema

--- my-app/server/models/user.js
+++ my-app/server/models/user.js
@@ -4,11 +4,14 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  profileImage: String,
+  profileImage: String, 
+  backgroundImage: String, 
+  introduction: String,
   followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   tweets: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tweet' }],
 });
+
 
 userSchema.plugin(passportLocalMongoose);
 const User = mongoose.model('User', userSchema);
