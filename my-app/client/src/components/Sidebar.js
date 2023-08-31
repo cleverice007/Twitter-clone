fada6efe92f5ec7ac24772e9917667672c7c24fd
cleverice007 <cleverice90128@gmail.com>
2023-08-31 14:04:57 +0800

    not using tailwind

--- my-app/client/src/components/Sidebar.js
+++ my-app/client/src/components/Sidebar.js
@@ -8,22 +8,22 @@ const Sidebar = () => {
   const location = useLocation();
   const currentPage = location.pathname.split('/').pop();
 
-  const isCurrentPage = (pageName) => currentPage === pageName ? 'text-blue-600' : '';
+  const isCurrentPage = (pageName) => currentPage === pageName ? styles.active : '';
 
   return (
-    <div className="bg-gray-900 text-white p-4">
-      <ul className="space-y-4">
-        <li className={`hover:bg-blue-800 rounded-lg p-2 ${isCurrentPage('home')}`} onClick={() => navigate('/home')}>
-          <span className="text-lg font-semibold">🏠</span>首頁
+    <div>
+      <ul>
+        <li className={`${isCurrentPage('home')} ${styles.item}`} onClick={() => navigate('/home')}>
+          <span className={`${styles.icon} ${styles.iconBlack}`}>🏠</span>首頁
         </li>
-        <li className={`hover:bg-blue-800 rounded-lg p-2 ${isCurrentPage('editprofile')}`} onClick={() => navigate('/editprofile')}>
-          <span className="text-lg font-semibold">👤</span>個人資料
+        <li className={`${isCurrentPage('editprofile')} ${styles.item}`} onClick={() => navigate('/editprofile')}>
+          <span className={`${styles.icon} ${styles.iconBlack}`}>👤</span>個人資料
+
         </li>
       </ul>
     </div>
   );
 };
-
 
 export default Sidebar;
 
