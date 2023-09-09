f3f635c1d7be044d2499f18f190016fa63ce069d
cleverice007 <cleverice90128@gmail.com>
2023-09-09 16:10:29 +0800

    remove aws confiq update part (那是sdk v2

--- my-app/server/routes/authRoutes.js
+++ my-app/server/routes/authRoutes.js
@@ -12,11 +12,7 @@ const path = require('path');
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const multerS3 = require('multer-s3');
 
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID, 
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION
-});
+
 
 const s3 = new S3Client({
   region: process.env.AWS_REGION,
@@ -26,11 +22,10 @@ const s3 = new S3Client({
   },
 });
 
-
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: 'some-bucket',
+    bucket: 'twitter-clone-mason',
     metadata: function (req, file, cb) {
       cb(null, { fieldName: file.fieldname });
     },
