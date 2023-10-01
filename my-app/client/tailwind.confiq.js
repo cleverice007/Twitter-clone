711d9bc7c2f0b25f3969ee4d5292c1ef573d7be4
cleverice007 <cleverice90128@gmail.com>
2023-10-01 21:07:18 +0800

    引入tailwind.css

---
+++ my-app/client/tailwind.confiq.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './src/**/*.html',
+      './src/**/*.js',
+      './src/**/*.jsx', // for React
+      './src/**/*.ts',  // for TypeScript
+      './src/**/*.tsx', // for TypeScript React
+      // etc.
+    ],  theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
